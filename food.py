82d9392c8325cfc00d884d9a2ffe5b6a0f6f34c2
momed_ali01 <demahomali01@gmail.com>
2024-08-12 11:50:46 +0300

    increased the high score in data.txt and also increased the range of food

--- food.py
+++ food.py
@@ -11,7 +11,7 @@ class Food(Turtle):
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(a=-330, b=330)
+        random_x = random.randint(a=-380, b=380)
         random_y = random.randint(a=-280, b=280)
         self.goto(random_x, random_y)
 
